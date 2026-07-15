ec4103080c6a802c72678882c7afdd299ebd7464
jgtans <jgtan@mail.ru>
2026-07-15 13:27:15 +0500

    установка sql

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 # Создали объект класса flask, для работы с веб-приложением (передаем базовый аргумент __name__)
 app = Flask(__name__)
-
-notes_db = {}
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/notes")
-def notes():
-    return render_template("notes.html", notes=notes_db)
-
-@app.route("/prog")
-def prog():
-    return render_template("programmer_diary.html")
-
-@app.route("/add_note", methods=["POST"])
-def add_note():
-    title = request.form.get("title")
-    text = request.form.get("text")
-
-    if title and text:
-        notes_db[title] = text
-
-    return redirect(url_for("notes"))
-
-@app.route("/submit", methods=["POST"])
-def submit():
-    name = request.form["name"]
-    return f"Привет {name}"
 
 if __name__ == "__main__":
     app.run(debug=True)
